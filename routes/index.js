7d1146f1e63e992c9045a10fd2ad6d680212e17a
baptistejesse@gmail.com <baptistejesse@yahoo.com>
2013-12-08 16:17:12 -0500

    Again

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 
+//mongod for mongoosejs on localhost
+
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'St.Albans Site' });
 };
